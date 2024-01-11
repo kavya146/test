c83f4f62d0fed5de6021f8de098e16548b40ff0b
kavya singampalli <singampallikavya01@gmail.com>
2024-01-11 15:41:27 +0530

    table content

--- new-app/src/component/maincontainer/MainCointainer.js
+++ new-app/src/component/maincontainer/MainCointainer.js
@@ -2,6 +2,8 @@ import "./maincontainer.css";
 import Header from "./header/Header";
 import Overview from "./overview/Overview";
 import TableContainerMain from "./tablecontainer/TableContainerMain";
+import searchlogo from "../maincontainer/header/searchlogo.svg";
+import sort from "./sort.svg";
 const MainContainer = () => {
   return (
     <div className="maincontainer">
@@ -9,6 +11,74 @@ const MainContainer = () => {
       <div style={{ margin: "30px" }}>
         <Overview />
       </div>
+      <div style={{ margin: "10px 0px 20px 70px" }}>
+        <h2>Transactions | This Month</h2>
+      </div>
+
+      <div style={{ margin: "30px 0px 20px 70px", display: "flex" }}>
+        <div>
+          <span
+            style={{
+              backgroundColor: "#e6e6e6",
+              width: "118px",
+              height: "32px",
+              borderRadius: "40px",
+              padding: "10px 16px",
+              textAlign: "center",
+              marginRight: "30px",
+            }}
+          >
+            Payouts (22)
+          </span>
+        </div>
+        <div style={{ cursor: "pointer" }}>
+          <span
+            style={{
+              backgroundColor: "#286ab0",
+              width: "148px",
+              height: "32px",
+              borderRadius: "40px",
+              padding: "10px 16px",
+              textAlign: "center",
+              marginRight: "30px",
+              color: "white",
+            }}
+          >
+            Refunds (6)
+          </span>
+        </div>
+      </div>
+
+      <div
+        style={{
+          margin: "10px 70px 10px 40px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <img
+            style={{ position: "relative", left: "50px" }}
+            src={searchlogo}
+          />
+          <input
+            style={{
+              width: "400px",
+              height: "40px",
+              textAlign: "center",
+              margin: "10px",
+              borderRadius: "6px",
+            }}
+            type="text"
+            placeholder="Order ID or transaction ID"
+          />
+        </div>
+        <div>
+          <img src={sort} />
+        </div>
+      </div>
+
       <TableContainerMain />
     </div>
   );
--- new-app/src/component/maincontainer/tablecontainer/TableContainerMain.js
+++ new-app/src/component/maincontainer/tablecontainer/TableContainerMain.js
@@ -1,5 +1,26 @@
 const TableContainerMain = () => {
-  return <div></div>;
+  return (
+    <div style={{ margin: "0px 70px 0px 70px", border: "5px" }}>
+      <table
+        style={{ width: "100%", textAlign: "left", backgroundColor: "white" }}
+      >
+        <tr style={{ backgroundColor: "#f2f2f2", height: "50px" }}>
+          <th>Order ID</th>
+          <th>Status</th>
+          <th>Transaction ID</th>
+          <th>Refund date</th>
+          <th>Order amount</th>
+        </tr>
+        <tr style={{ height: "50px" }}>
+          <td>#2234565</td>
+          <td>Successful</td>
+          <td>1234567765456</td>
+          <td>Today, 08:45 PM</td>
+          <td>23556.34</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default TableContainerMain;
