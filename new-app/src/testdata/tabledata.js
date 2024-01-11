3241ad98f7c98a53414454961575c00a84d18908
kavya singampalli <singampallikavya01@gmail.com>
2024-01-11 18:54:16 +0530

    table content/mock new"

--- new-app/src/testdata/tabledata.js
+++ new-app/src/testdata/tabledata.js
@@ -20,6 +20,13 @@ const dummydata = [
     refundDate: "Today, 08:45 PM",
     orderAmount: 1234.43,
   },
+  {
+    orderId: 281209,
+    status: "Successful",
+    transactionId: 12345676,
+    refundDate: "Today, 08:45 PM",
+    orderAmount: 1234.43,
+  },
 ];
 
 export default dummydata;
